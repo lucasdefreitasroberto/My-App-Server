75433fbc80e7b5c7a7daa63fbdd5f064723e8b08
lucasdefreitasroberto <68399974+lucasdefreitasroberto@users.noreply.github.com>
2023-04-02 14:41:49 -0300

    Update livro.js
    Creating the server base, for library application

--- Rotas/livro.js
+++ Rotas/livro.js
@@ -6,7 +6,19 @@ router.get('/', (req, res) => {
 })
 
 router.get('/ovo', (req, res) => {
-    res.send('index.js');
+    res.send('OVO');
+})
+
+router.post('/', (req, res) => {
+    res.send('Foi feito uma requisição Post')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Foi feito uma requisição Patch')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Foi feito uma requisição Delete')
 })
 
 module.exports = router;
